6f968579d9e6a8855e4eb5635484393632ed9c91
Ilia-Pohorielov <ilia.pohorielov@gmail.com>
2018-09-30 18:12:49 +0300

    react-online-3-lesson-5

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -5,19 +5,41 @@ import React, { Component } from 'react';
 import Composer from '../Composer';
 import Post from '../Post';
 import StatusBar from "../StatusBar";
+import Spinner from 'components/Spinner';
 
 // Instruments
 import Styles from './styles.m.css';
 
 
 export default class Feed extends Component {
+    state = {
+        posts: [
+            {
+                id: '1',
+                comment: 'Hi there',
+                created: 1526825076849,
+            },
+            {
+                id: '2',
+                comment: 'Привет!',
+                created: 1526825076855,
+            }
+        ],
+        isSpinning : true,
+    };
     render(){
+        const { posts, isSpinning } = this.state;
+
+        const postsJSX = posts.map((post) => {
+           return <Post key = { post.id } { ...post } />;
+        });
 
         return (
-            <section className={ Styles.feed }>
+            <section className ={ Styles.feed }>
+                <Spinner isSpinning = { isSpinning } />
                 <StatusBar />
                 <Composer />
-                <Post />
+                { postsJSX }
             </section>
         );
     }
--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -1,29 +1,37 @@
 // Core
 import React, { Component } from 'react';
 import moment from 'moment';
+import PropTypes from 'prop-types';
 
 // Components
 import { Consumer } from 'components/HOC/withProfile';
 
 
 // Instruments
-import avatar from '../../theme/assets/lisa';
 import Styles from './styles.m.css';
 
 export default class Feed extends Component {
+    static propTypes = {
+        comment: PropTypes.string.isRequired,
+        created: PropTypes.number.isRequired,
+    };
+
     render(){
+
+        const { comment, created } = this.props;
+
         return (
             <Consumer>
                 {
                     (context) => (
                         <section className={ Styles.post }>
-                            <img src={ context.avatar } alt = { context.currentUserFirstName } />
-                            <a href="#">{`
-                    ${ context.currentUserFirstName } ${ context.currentUserLastName }
-                    `}
+                            <img src ={ context.avatar } alt = { context.currentUserFirstName } />
+                            <a href ="#">{`
+                                    ${ context.currentUserFirstName } ${ context.currentUserLastName }
+                                `}
                             </a>
-                            <time>{moment().format('MMMM D h:mm:ss a')}</time>
-                            <p>Howdy!</p>
+                            <time>{moment.unix(created).format('MMMM D h:mm:ss a')}</time>
+                            <p>{ comment }</p>
                         </section>
                     )
                 }
